8765519f416f880aca54b85bf3181136f21e27c4
Will Beasley <wibeasley@hotmail.com>
2015-05-22 23:03:16 -0500

    moving over `install_packages()`

---
+++ R/install_packages.R
@@ -0,0 +1,81 @@
+#This code checks the user's installed packages against the packages listed in `./utility/package_dependency_list.csv`.
+#   These are necessary for the repository's R code to be fully operational.
+#   CRAN packages are installed only if they're not already; then they're updated if available.
+#   GitHub packages are installed regardless if they're already installed.
+#If anyone encounters a package that should be on there, please add it to `./utility/package_dependency_list.csv`
+
+install_packages <- function(
+                            path_csv = './utility/package_dependency_list.csv',
+                            cran_repo = "http://cran.rstudio.com",
+                            check_libcurl_linux = (R.Version()$os=="linux-gnu")
+                          ) {
+
+  if( !file.exists(path_csv))
+    base::stop("The path `", path_csv, "` was not found.  Make sure the working directory is set to the root of the repository.")
+  #####################################
+  ## load_data
+  ds_packages <- utils::read.csv(file=path_csv, stringsAsFactors=FALSE)
+  
+  rm(path_csv)
+  #####################################
+  ##  tweak_data
+  ds_install_from_cran <- ds_packages[ds_packages$install & ds_packages$on_cran, ]
+  ds_install_from_github <- ds_packages[ds_packages$install & !is.na(ds_packages$github_username) & nchar(ds_packages$github_username)>0, ]
+  
+  rm(ds_packages)
+  #####################################
+  ## update_cran_packages
+  utils::update.packages(ask=FALSE, checkBuilt=TRUE)
+  
+  #####################################
+  ## install_devtools
+  # Installing the devtools package is different than the rest of the packages.  On Windows,
+  #   the dll can't be overwritten while in use.  This function avoids that issue.
+  # This should follow the initial CRAN installation of `devtools`.
+  #   Installing the newest GitHub devtools version isn't always necessary, but it usually helps.
+  
+  if( !base::requireNamespace("devtools") )
+    utils::install.packages("devtools", repos=cran_repo)
+
+  #####################################
+  ## install_cran_packages
+  for( package_name in ds_install_from_cran$package_name ) {
+    available <- base::requireNamespace(package_name, quietly=TRUE) #Checks if it's available
+    if( !available ) {
+      utils::install.packages(package_name, dependencies=TRUE)
+      #base::requireNamespace( package_name, character.only=TRUE)
+    } else {
+      #Make sure all their dependencies are installed & up-to-date
+      update(devtools::package_deps(package_name, dependencies=TRUE)) #devtools:::update.package_deps
+    }
+    base::rm(available)
+  }
+  
+  rm(ds_install_from_cran, package_name)
+  #####################################
+  ## check_for_libcurl
+  
+  if( check_libcurl_linux ) {
+    libcurl_results <- base::system("locate libcurl4")
+    libcurl_missing <- (libcurl_results==0)
+    
+    if( libcurl_missing )
+      base::warning("This Linux machine is possibly missing the 'libcurl' library.  ",
+                    "Consider running `sudo apt-get install libcurl4-openssl-dev`.")
+    
+    base::rm(libcurl_results, libcurl_missing)
+  }
+  
+  #####################################
+  ## install_github_packages
+  
+  for( i in base::seq_len(base::nrow(ds_install_from_github)) ) {
+    package_name <- ds_install_from_github[i, "package_name"]
+    username <- ds_install_from_github[i, "github_username"]
+    repository_name <- paste0(username, "/", package_name)
+    devtools::install_github(repo=repository_name)
+    base::rm(package_name, username, repository_name)
+  }
+  
+  base::rm(ds_install_from_github, i)
+}
