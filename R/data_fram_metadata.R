60f952b655cc99e7bd035725caa510b865a964b4
Will Beasley <wibeasley@hotmail.com>
2015-05-26 14:58:12 -0500

    rename headstart pads and aligns the output

--- R/data_fram_metadata.R
+++ R/data_fram_metadata.R
@@ -1,5 +1,12 @@
 column_rename_headstart <- function( d ) {
-  cat(paste0(", \"", colnames(d), "\"", " = ", "\"", colnames(d), "\"", "\n")) #Gives a headstart to plyr::rename
+  max_column_name <- max(nchar(colnames(d)))
+  extra_character_length <- 5L #a comma, two quotes, and two backslashes.
+  
+  left_side <- paste0(", \"", colnames(d), "\"")
+  padded_format <- paste0("%-", max_column_name + extra_character_length, "s")
+  left_side <- sprintf(padded_format, left_side)
+  
+  cat(paste0(left_side, " = ", "\"", colnames(d), "\"", "\n")) #Gives a headstart to plyr::rename
 }
 # column_rename_headstart(ds)
 
