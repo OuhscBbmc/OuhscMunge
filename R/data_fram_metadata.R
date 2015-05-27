1581eef6d5dfbb24cb7b9a6f3f4a3e4238251c1b
Will Beasley <wibeasley@hotmail.com>
2015-05-26 19:52:43 -0500

    headstart for class & value

--- R/data_fram_metadata.R
+++ R/data_fram_metadata.R
@@ -6,20 +6,35 @@ column_rename_headstart <- function( d ) {
   padded_format <- paste0("%-", max_column_name + extra_character_length, "s")
   left_side <- sprintf(padded_format, left_side)
   
-  cat(paste0(left_side, " = ", "\"", colnames(d), "\"", "\n")) #Gives a headstart to plyr::rename
+  right_side <- paste0("\"", colnames(d), "\"\n")
+  
+  cat(paste0(left_side, " = ", right_side)) #Gives a headstart to plyr::rename
 }
 # column_rename_headstart(ds)
 
 column_class_headstart <- function( d ) {
-  for( i in seq_along(colnames(d)) ) {
-    cat( ", \"", colnames(d)[i], "\" = \"", class(d[, i]), "\"\n", sep="")
-  }
+  max_column_name <- max(nchar(colnames(d)))
+  extra_character_length <- 5L #a comma, two quotes, and two backslashes.
+  
+  left_side <- paste0(", \"", colnames(d), "\"")
+  padded_format <- paste0("%-", max_column_name + extra_character_length, "s")
+  left_side <- sprintf(padded_format, left_side)
+  
+  right_side <- paste0("\"", sapply(d, class), "\"\n")
+  
+  cat(paste0(left_side, " = ", right_side))
 }
 # column_class_headstart(ds)
 
 column_value_headstart <- function( x ) {
-  for( value in sort(unique(x)) ) {
-    cat( ", \"", value, "\" = \"", value, "\"\n", sep="")
-  }
+  values <- sort(unique(x))
+  max_value_length <- max(nchar(values))
+  extra_character_length <- 5L #a comma, two quotes, and two backslashes.
+  
+  left_side <- paste0(", \"", values, "\"")
+  padded_format <- paste0("%-", max_value_length + extra_character_length, "s")
+  left_side <- sprintf(padded_format, left_side)
+  
+  cat(paste0(left_side, " = \"", values, "\"\n"))
 }
 # column_value_headstart(ds$Activity)
