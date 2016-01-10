f7a2888761d130ddefecb66f9acd7e1fdc50f38a
Will Beasley <wibeasley@hotmail.com>
2016-01-10 00:31:29 -0600

    Verbose janitor.

--- tests/testthat/test-package-janitor.R
+++ tests/testthat/test-package-janitor.R
@@ -6,5 +6,5 @@ context("Package Janitor")
 
 test_that("Smoke test w/o update", {
   path <- base::file.path(devtools::inst(name="OuhscMunge"), "package-dependency-list.csv")
-  OuhscMunge:::package_janitor(path_package_dependencies=path, update_packages=FALSE)
+  OuhscMunge:::package_janitor(path_package_dependencies=path, update_packages=FALSE, verbose=TRUE)
 })
