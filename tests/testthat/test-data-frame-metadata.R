23630abda0b6164b8b0317c067b9652c44a182c7
Will Beasley <wibeasley@hotmail.com>
2016-03-19 15:18:34 -0500

    More checks of column rename

--- tests/testthat/test-data-frame-metadata.R
+++ tests/testthat/test-data-frame-metadata.R
@@ -17,7 +17,35 @@ test_that("column rename -OrchardSprays", {
   )
 })
 
-test_that("column rename -OrchardSprays", {
+test_that("column rename -Iris", {
+  expected <-
+', "sepal_length"            = "`Sepal.Length`"
+ , "sepal_width"             = "`Sepal.Width`"
+ , "petal_length"            = "`Petal.Length`"
+ , "petal_width"             = "`Petal.Width`"
+ , "species"                 = "`Species`"'
+  
+  testthat::expect_output(
+    column_rename_headstart(datasets::iris)
+    , expected
+  )
+})
+
+
+test_that("column rename w/o snake-Iris", {
+  expected <-
+', "Sepal.Length"            = "`Sepal.Length`"
+ , "Sepal.Width"             = "`Sepal.Width`"
+ , "Petal.Length"            = "`Petal.Length`"
+ , "Petal.Width"             = "`Petal.Width`"
+ , "Species"                 = "`Species`"'
+  
+  testthat::expect_output(
+    column_rename_headstart(datasets::iris, try_snake_case=FALSE)
+    , expected
+  )
+})
+test_that("column class -OrchardSprays", {
   expected <-
 ', "decrease"   = "numeric"
  , "rowpos"     = "numeric"
@@ -26,6 +54,19 @@ test_that("column rename -OrchardSprays", {
   
   testthat::expect_output(
     column_class_headstart(datasets::OrchardSprays)
+    , expected
+  )
+})
+test_that("column class -Iris", {
+  expected <-
+', "Sepal.Length"  = "numeric"
+ , "Sepal.Width"   = "numeric"
+ , "Petal.Length"  = "numeric"
+ , "Petal.Width"   = "numeric"
+ , "Species"       = "factor"'
+  
+  testthat::expect_output(
+    column_class_headstart(datasets::iris)
     , expected
   )
 })
