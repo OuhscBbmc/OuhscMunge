ba5de91372358bd52c1ca61a34b1e53d7ca53df7
Will Beasley <wibeasley@hotmail.com>
2017-07-02 16:21:27 -0500

    trim_date
    See #29

--- tests/testthat/test-trim.R
+++ tests/testthat/test-trim.R
@@ -1,10 +1,7 @@
 library(testthat)
 
-###########
-context("Trim Numeric")
-###########
-
 # ---- trim_numeric() -------------------------------------------------------
+context("Trim Numeric")
 
 test_that("trim_numeric -good", {
   x           <- c(NA, -4:10) / 10
@@ -50,11 +47,9 @@ test_that("trim_numeric -bad values", {
     "Assertion on 'bounds' failed: Must have length <= 2, but has length 3."
   )
 })
-###########
-context("Trim Integer")
-###########
 
 # ---- trim_integer() -------------------------------------------------------
+context("Trim Integer")
 
 test_that("trim_integer -good", {
   x           <- c(NA, -4:10) 
@@ -97,6 +92,54 @@ test_that("trim_integer -bad values", {
   
   expect_error(
     OuhscMunge::trim_integer(x, c(1L, 2L, 3L)),
+    "Assertion on 'bounds' failed: Must have length <= 2, but has length 3."
+  )
+})
+
+# ---- trim_date() -------------------------------------------------------
+context("Trim Date")
+
+test_that("trim_date -good", {
+  x           <- as.Date(c("1902-02-02", "1999-09-09", "2020-02-22", "2030-01-01", "2030-01-02"))
+  bounds      <- as.Date(c("1990-01-01", "2030-01-01"))
+  replacement <- as.Date("1977-07-27")
+  
+  expected_1  <-as.Date(c(NA_character_, "1999-09-09", "2020-02-22", "2030-01-01", NA_character_))
+  expected_2  <-as.Date(c("1977-07-27", "1999-09-09", "2020-02-22", "2030-01-01", "1977-07-27"))
+  
+  observed_1  <- OuhscMunge::trim_date(x, bounds)
+  observed_2  <- OuhscMunge::trim_date(x, bounds, replacement)
+   
+  expect_equal(observed_1, expected_1)
+  expect_equal(observed_2, expected_2)
+  
+  expect_equal(class(observed_1), "Date", "The returned array should remain a double-precision data type.")
+  expect_equal(class(observed_2), "Date", "The returned array should remain a double-precision data type.")
+})
+
+test_that("trim_date -degenerate", {
+  bounds      <- as.Date(c("1990-01-01", "2030-01-01"))
+
+  expect_equal(OuhscMunge::trim_date(as.Date(NA_character_), bounds), as.Date(NA_character_))
+  expect_equal(OuhscMunge::trim_date(as.Date(character(0)), bounds), as.Date(character(0)))
+})
+
+test_that("trim_date -bad values", {
+  x           <- as.Date(c("1902-02-02", "1999-09-09", "2020-02-22", "2030-01-01", "2030-01-02"))
+  bounds      <- as.Date(c("1990-01-01", "2030-01-01"))
+  replacement <- as.Date("1977-07-27")
+  
+  expect_error(
+    OuhscMunge::trim_date(x, as.Date(NA_character_)),
+    "Assertion on 'bounds' failed: Must have length >= 2, but has length 1."
+  )
+  expect_error(
+    OuhscMunge::trim_date(x, as.Date(c("2030-01-01", "1990-01-01"))),
+    "The lower element of `bounds` must be equal or less than the upper element of `bounds`."
+  )
+  
+  expect_error(
+    OuhscMunge::trim_date(x,  as.Date(c("1990-01-01", "2010-01-01", "2030-01-01"))),
     "Assertion on 'bounds' failed: Must have length <= 2, but has length 3."
   )
 })
