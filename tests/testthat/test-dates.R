b627231c2cb4a3744a3ea6dea811fedf839ccaa8
Will Beasley <wibeasley@hotmail.com>
2017-07-12 10:33:09 -0500

    tidy dates
    See #31

--- tests/testthat/test-dates.R
+++ tests/testthat/test-dates.R
@@ -5,10 +5,11 @@ context("dates-month")
 
 test_that("clump_month_date -typical", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
-  expected <- structure(c(15079, 15079, 15079, 15079, 15079, 15079, 15079, 
-                          15079, 15079, 15079, 15109, 15109, 15109, 15109, 15109, 15109, 
-                          15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109), class = "Date")
-  observed <- OuhscMunge::clump_month_date(detailed)#; dput(clumped)
+  expected <- structure(c(
+    15079, 15079, 15079, 15079, 15079, 15079, 15079, 15079, 15079, 15079, 
+    15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109, 15109
+  ), class = "Date")
+  observed <- clump_month_date(detailed)#; dput(observed)
   
   expect_equal(observed, expected, label="The dates should be clumped corrected.")
   expect_equal(class(observed), "Date", "The returned array should remain a date data type.")
@@ -17,7 +18,7 @@ test_that("clump_month_date -typical", {
 test_that("clump_month_date -not a date", {
   detailed <- letters
   expect_error(
-    OuhscMunge::clump_month_date(detailed)
+    clump_month_date(detailed)
     , regexp = "The `date_detailed` parameter must be a Date, POSIXct, or POSIXlt data type\\."
   )
 })
@@ -25,7 +26,7 @@ test_that("clump_month_date -not a date", {
 test_that("clump_month_date -bad day character", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
   expect_error(
-    OuhscMunge::clump_month_date(detailed, day_of_month="a")
+    clump_month_date(detailed, day_of_month="a")
     , regexp = "The `day_of_month` parameter must be an integer or numeric data type\\."
   )
 })
@@ -33,7 +34,7 @@ test_that("clump_month_date -bad day character", {
 test_that("clump_month_date -day not a scalar", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
   expect_error(
-    OuhscMunge::clump_month_date(detailed, day_of_month=1:4)
+    clump_month_date(detailed, day_of_month=1:4)
     , regexp = "The `day_of_month` contains more than one element; it should contain only one\\."
   )
 })
@@ -41,7 +42,7 @@ test_that("clump_month_date -day not a scalar", {
 test_that("clump_month_date -bad day 32", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
   expect_error(
-    OuhscMunge::clump_month_date(detailed, day_of_month=32)
+    clump_month_date(detailed, day_of_month=32)
     , regexp = ".*?The `day_of_month` parameter must be bound by \\[1, 31]\\."
   )
 })
@@ -49,7 +50,7 @@ test_that("clump_month_date -bad day 32", {
 test_that("clump_month_date -bad day 31.5", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
   expect_error(
-    OuhscMunge::clump_month_date(detailed, day_of_month=31.5)
+    clump_month_date(detailed, day_of_month=31.5)
     , regexp = ".*The `day_of_month` parameter must be bound by \\[1, 31]\\."
   )
 })
@@ -61,11 +62,12 @@ context("dates-week")
 test_that("clump_week_date -typical", {
   detailed <- seq.Date(from=as.Date("2011-04-21"), to=as.Date("2011-05-14"), by="day")
   expected <- structure(c(
-    15082, 15082, 15082, 15089, 15089, 15089, 15089, 
-    15089, 15089, 15089, 15096, 15096, 15096, 15096, 15096, 15096, 
-    15096, 15103, 15103, 15103, 15103, 15103, 15103, 15103
+    15082, 15082, 15082, 
+    15089, 15089, 15089, 15089, 15089, 15089, 15089, 
+    15096, 15096, 15096, 15096, 15096, 15096, 15096, 
+    15103, 15103, 15103, 15103, 15103, 15103, 15103
   ), class = "Date")
-  observed <- OuhscMunge::clump_week_date(detailed)#; dput(observed)
+  observed <- clump_week_date(detailed)#; dput(observed)
   
   expect_equal(observed, expected, label="The dates should be clumped corrected.")
   expect_equal(class(observed), "Date", "The returned array should remain a date data type.")
