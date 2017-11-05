ac46bccf3c8c3d949f97434a164a982577b3fca0
Will Beasley <wibeasley@hotmail.com>
2017-11-05 18:53:27 -0500

    `deterge_to_ascii()`
    The original name was `clean_to_ascii()`, but this is more consistent with the othe rpackage functions.  Closes #40

--- R/deterge.R
+++ R/deterge.R
@@ -1,5 +1,5 @@
 #' @name deterge
-#' @aliases deterge_to_double deterge_to_integer
+#' @aliases deterge_to_double deterge_to_integer deterge_to_ascii
 #' @title Convert (and possibly clean) a vector
 #'  
 #' @description Cast values to desired data type.
@@ -7,23 +7,35 @@
 #' @usage 
 #' deterge_to_double(x, bound_lower, bound_upper)
 #' deterge_to_integer(x, bound_lower, bound_upper)
+#' deterge_to_ascii(x, substitution_character)
 #' 
-#' @param x The input vector that needs to be cast.  Required
+#' @param x The input vector that needs to be cast/converted.  Required.
 #' @param bound_lower Elements below this inclusive threshold will be set to `NA`.
 #' @param bound_upper Elements above this inclusive threshold will be set to `NA`.
+#' @param substitution_character If the character does not have an equivalent in ASCII,
+#' replace it with this character.  Defaults to a question mark (*i.e.*, '?').
 #' 
 #' @return An array of values.
 #' 
 #' @details 
-#' Accepts character representations of a number, and returns a `numeric` or `integer`
+#' The functions `deterge_to_double()` and `deterge_to_integer()` accept character representations of a number, and return a `numeric` or `integer`
 #' vector.  Elements outside `bound_lower` and `bound_upper` are converted to `NA_real_`/`NA_integer_`.
 #' 
+#' The function `deterge_to_ascii()` accepts a character vector and returns a character vector.
+#' The encoding is changed to ASCII.  Individual elements are allowed to be `NA_character_`.
+#' 
 #' @author Will Beasley
+#' 
+#' @seealso The real work in `deterge_to_ascii()` is performed by [`base::iconv()`].
+#' `base::iconv(x=x, from="latin1", to="ASCII//TRANSLIT", sub=substitution_character)`
 #' 
 #' @examples
 #' library(OuhscMunge)
 #' deterge_to_double(c(NA, 1:10), 4, 8)
 #' deterge_to_integer(c(NA, 1:10), 4L, 8L)
+#' 
+#' x <- c("Ekstr\xf8m", "J\xf6reskog", "bi\xdfchen Z\xfcrcher")
+#' deterge_to_ascii(x)
 
 
 #' @export
@@ -45,8 +57,6 @@ deterge_to_double <- function( x, bound_lower=-Inf, bound_upper=Inf ) {
 #' @export
 deterge_to_integer <- function( x, bound_lower=-2147483647L, bound_upper=2147483647L ) {
   
-  # checkmate::assert_number(bound_lower, mi
-  
   if( !(class(bound_lower) %in% c("numeric", "integer") & length(bound_lower)==1L) ) 
     stop("The parameter `bound_lower` must be a numeric or integer vector with exactly one element.")
   if( !(class(bound_upper) %in% c("numeric", "integer") & length(bound_upper)==1L) ) 
@@ -57,4 +67,17 @@ deterge_to_integer <- function( x, bound_lower=-2147483647L, bound_upper=2147483
   
   # Set values that are outside the thresholds to NA.
   trim_integer(x, c(bound_lower, bound_upper))
+}
+
+#' @export
+deterge_to_ascii <- function( x, substitution_character="?" ) {
+  
+  # Verify the vector is a `character`.
+  checkmate::assert_character(x, null.ok=FALSE,  any.missing=FALSE)
+  
+  # Verify the substitution value is a single, nonmissing character.
+  checkmate::assert_character(substitution_character, len=1L, any.missing=FALSE)
+  
+  # Convert encoding.
+  base::iconv(x=x, from="latin1", to="ASCII//TRANSLIT", sub=substitution_character)
 }
