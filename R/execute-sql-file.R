fb35ef7782109f98400e5123d18e64914e5a389c
Will Beasley <wibeasley@hotmail.com>
2018-03-28 00:03:15 -0500

    accommodate returning a dataset
    closes #44.

--- R/execute-sql-file.R
+++ R/execute-sql-file.R
@@ -7,6 +7,10 @@
 #'
 #' @param path_sql A vector to of names to convert.  Required character.
 #' @param dsn The name of a [DSN](https://en.wikipedia.org/wiki/Data_source_name) defined on your local machine  Required character.
+#' @param execute Indicates if `DBI::dbExecute()` should be used
+#' (which typically returns a scalar).  Otherwise, `DBI::dbGetQuery()` is used,
+#' (which will return a `tibble::tibble`).  Required `logical`.
+#' @param minimum_row_count If `execute` is false, the returned dataset should have at least this many rows, or an error will be thrown.  Default of 0.  Required integer.
 #'
 #' @return A vector of converted names.
 #'
@@ -18,17 +22,25 @@
 #' execute_sql_file("inst/condense-date.sql", "cdw_cache_staging")
 #' }
 
-execute_sql_file <- function( path_sql, dsn ) {
+execute_sql_file <- function( path_sql, dsn, execute=TRUE, minimum_row_count=0L ) {
   checkmate::expect_file_exists(path_sql)
   checkmate::assert_character(path_sql, min.chars=2, min.len=1, max.len=1, any.missing=F)
   checkmate::assert_character(dsn     , min.chars=2, min.len=1, max.len=1, any.missing=F)
+  checkmate::assert_logical(  execute              , len=1, any.missing=F)
 
   sql   <- readr::read_file(path_sql)
   checkmate::assert_character(sql     , min.chars=2, min.len=1, max.len=1, any.missing=F)
 
   tryCatch({
     channel <- odbc::dbConnect(odbc::odbc(), dsn)
-    returned_value <- DBI::dbExecute(channel, sql)
+    if( execute ) {
+      returned_value <- DBI::dbExecute(channel, sql)
+    } else {
+      returned_value <- DBI::dbGetQuery(channel, sql) %>%
+        tibble::as_tibble()
+
+      OuhscMunge::verify_data_frame(returned_value, minimum_row_count=minimum_row_count)
+    }
   }, finally={
     odbc::dbDisconnect(channel)
   })
