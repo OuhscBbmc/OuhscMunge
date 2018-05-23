6c9a113bf233d32931b7357b557611e8fc9ec14f
Will Beasley <wibeasley@hotmail.com>
2018-05-23 02:10:43 -0500

    install packages addin
    still need tests though.  ref #66

---
+++ R/install-packages-addin.R
@@ -0,0 +1,11 @@
+#' Install important packages
+#'
+#' Installs the [important packages](https://github.com/OuhscBbmc/RedcapExamplesAndPatterns/blob/master/utility/package-dependency-list.csv)
+#' typically used by [BBMC](https://github.com/OuhscBbmc) data analysts.
+#'
+#' @export
+install_packages_addin <- function() {
+  if( !base::requireNamespace("devtools") ) utils::install.packages("devtools")
+  devtools::source_gist("2c5e7459b88ec28b9e8fa0c695b15ee3", filename="package-janitor-bbmc.R")
+  package_janitor_remote("https://raw.githubusercontent.com/OuhscBbmc/RedcapExamplesAndPatterns/master/utility/package-dependency-list.csv")
+}
