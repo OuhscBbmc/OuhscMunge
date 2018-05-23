c88cb7fee0fb1eecbd719d6b54131fff37c92d57
Will Beasley <wibeasley@hotmail.com>
2018-05-23 09:07:20 -0500

    skip tests for `update/install_packages_addin()`
    ref #66

--- tests/testthat/test-update-packages-addin.R
+++ tests/testthat/test-update-packages-addin.R
@@ -4,6 +4,7 @@ library(testthat)
 context("update_packages_addin")
 
 test_that("update_packages_addin", {
-  # update_packages_addin()
+  testthat::skip("This function is too invasive to run on other people's machines.")
+  update_packages_addin()
   testthat::expect_true(TRUE)
 })
