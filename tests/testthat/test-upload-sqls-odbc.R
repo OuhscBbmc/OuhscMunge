6667be890dd34daa0bd487a58485d4047a142533
Will Beasley <wibeasley@hotmail.com>
2018-06-11 00:46:06 -0500

    allow werker error to be different
    ref #69

--- tests/testthat/test-upload-sqls-odbc.R
+++ tests/testthat/test-upload-sqls-odbc.R
@@ -12,6 +12,6 @@ dsn_name      <- "pretend-dsn"
 test_that("upload_sqls_odbc -good arguments", {
   expect_error(
     upload_sqls_odbc(d, schema_name, table_name, dsn_name),
-    "Data source name not found and no default driver specified"
+    "Data source name not found,? and no default driver specified"
   )
 })
