91b6db383dc73fced6c98d49ff180f6ac4ec1554
Will Beasley <wibeasley@hotmail.com>
2020-01-08 19:38:38 -0600

    tidy
    ref #108

--- R/data-frame-stack.R
+++ R/data-frame-stack.R
@@ -2,22 +2,24 @@
 #'
 #' @title Verify equivalent structure of two dataset
 #'
-#' @description These functions help to compare two metadata frames as assess if new rows
-#' should be added.
-#'
+#' @description These functions help to compare two metadata frames and assess
+#' if new rows should be added.
 #'
 #' @param d_original A `data.frame` that serves as the existing metadata file
 #' that potentialy needs to be updated.  Required.
-#' @param d_current A `data.frame` that contains records potentialy missing from
-#' `d_original`. Required.
-#' @param keys Column names that represent unique combination. `character` vector. Optional.
+#' @param d_current A `data.frame` that contains records potentialy missing
+#' from `d_original`. Required.
+#' @param keys Column names that represent unique combination.
+#' `character` vector. Optional.
 #'
-#' @return A [`tibble::tibble`] that combines `d_original` with the new records from `d_current`.
+#' @return A [`tibble::tibble`] that combines `d_original` with the new records
+#' from `d_current`.
 #'
 #' @note each dataset is verified to not have more then one
 #' row with the same values in the combination of `keys`
 #'
 #' @seealso [data_frame_compare_structure()]
+#'
 #' @importFrom magrittr %>%
 #' @importFrom rlang .data
 #'
@@ -43,7 +45,6 @@
 #'   dplyr::anti_join(ds_original, by = c("x1", "x2"))
 #'
 #' @export
-
 data_frame_stack <- function(d_original, d_current, keys) {
 
   # Check arguments
@@ -81,21 +82,3 @@ data_frame_stack <- function(d_original, d_current, keys) {
   d_original %>%
     dplyr::union_all(d_new)
 }
-
-# import::from("magrittr", "%>%")
-# ds_original <- tibble::tibble(
-#   x1  = c(1, 3, 4),
-#   x2  = letters[c(1, 3, 4)],
-#   x3  = c(11, 13, 14)
-# )
-#
-# ds_current <- tibble::tibble(
-#   x1  = c(1:5, 1, 5),
-#   x2  = c(letters[1:5], "x", "y"),
-#   x3  = c(11, 12, 13, 14, 15, 11, 15)
-# )
-#
-# ds_new <- data_frame_compare_structure(ds_original, ds_current, c("x1", "x2"))
-#
-# ds_original %>%
-#   dplyr::union_all(ds_current)
