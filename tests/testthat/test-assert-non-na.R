463964e6d589f1fc3e676e06ed01654ddf001f03
Will Beasley <wibeasley@hotmail.com>
2020-01-12 12:33:53 -0600

    test wrong class

--- tests/testthat/test-assert-non-na.R
+++ tests/testthat/test-assert-non-na.R
@@ -43,6 +43,12 @@ test_that("assert_non_na -proportion - passes", {
   expect_silent(assert_non_na(c(NA, sample(c(T,F), size=100, replace = TRUE)                                     ) , "logical"   , 0.5))
   expect_silent(assert_non_na(c(as.Date(NA), seq.Date(as.Date("2015-01-02"), as.Date("2017-04-30"), by="days")   ) , "Date"      , 0.5))
 })
+test_that("assert_non_na -wrong class", {
+  error_pattern <- "The vector must inherit from the class `\\w+`, but it is a `\\w+`\\."
+  expect_error(assert_non_na(1:100, "character"   , 0.999))
+  expect_error(assert_non_na(1:100, "Date"        , 0.999))
+  expect_error(assert_non_na(1:100, "double"      , 0.999))
+})
 test_that("assert_non_na -proportion - fails", {
   error_pattern <- "The vector must have a proportion of at least 0\\.999 of nonmissing elements\\.  However the actual nonmissing proportion is 0\\.\\d+\\."
   expect_error(assert_non_na(c(NA, 1:100                                                                       ) , "integer"   , 0.999))
