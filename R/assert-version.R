01699b08e843052efcaffef3e69978dd33e50ad6
Will Beasley <wibeasley@hotmail.com>
2024-01-12 19:36:24 -0600

    `assert_version_r()`
    close #137

---
+++ R/assert-version.R
@@ -0,0 +1,58 @@
+#' @name assert_version
+#' @aliases assert_version_r
+#' @title I that the local machine is using an acceptable version.
+#'
+#' @description Assert that the local machine is using a version that satisfies
+#' the minimum version specified.
+#'
+#' @param minimum A [package_version] or [character] that specifies
+#' the version of the software being examined
+#' (ie, R, a package, or an ODBC driver).
+#'
+#' @return An error if the minimum version is not met.
+#' If the local machine is using an acceptable version, an `invisible()` `TRUE`
+#' is returned.
+#'
+#' @note These functions help us assert the the local machine has an acceptable
+#' version of the software running.
+#'
+#' For [assert_version_r()], the current default value is "4.2.0" because
+#' it introduced the
+#' [placeholder](https://davidbudzynski.github.io/general/2022/04/23/r-native-placeholder.html)
+#' for the native pipe.  Future versions of OuhscMunge will likely increase
+#' the default value to keep pace with important developments to R.
+#'
+#' @author Will Beasley
+#'
+#' @examples
+#' assert_version_r("3.1.0")
+#' assert_version_r()
+#' # Fails: assert_version_r("99.1.0")
+
+#' @export
+assert_version_r <- function(minimum = base::package_version("4.2.1")) {
+  checkmate::assert_vector(minimum, len = 1, any.missing = FALSE)
+  v <-
+    if (inherits(minimum, "package_version")) {
+      as.character(minimum)
+    } else if (inherits(minimum, "character")) {
+      # Make sure it can be recognized as a version
+      as.character(base::package_version(minimum))
+    } else {
+      stop("The value passed to `minimum` must inherit either from 'character' or `package_version`.")
+    }
+
+  current <- as.character(utils::packageVersion("base"))
+
+  comparison <-
+    utils::compareVersion(
+      current,
+      v
+    )
+
+  if (comparison < 0 ) {
+    stop("Your R version is too old.  Update it at <https://cloud.r-project.org>.")
+  } else {
+    TRUE
+  }
+}
