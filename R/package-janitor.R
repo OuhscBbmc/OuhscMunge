15b3be49365469b09fa6a8c0bc9667f0d8153f7e
Will Beasley <wibeasley@hotmail.com>
2026-08-15 17:47:11 -0500

    rely on more pak functionality

--- R/package-janitor.R
+++ R/package-janitor.R
@@ -16,7 +16,6 @@
 #'     if( !base::requireNamespace("devtools") ) utils::install.packages("devtools")
 #'     devtools::source_gist("https://gist.github.com/wibeasley/2c5e7459b88ec28b9e8fa0c695b15ee3")
 #'     package_janitor_remote("https://raw.githubusercontent.com/OuhscBbmc/RedcapExamplesAndPatterns/main/utility/package-dependency-list.csv")
-
 #' @name package_janitor_remote
 #' @export
 #'
@@ -61,147 +60,148 @@
 #' }
 
 package_janitor_remote <- function(
-  url_package_dependencies,
-  cran_repo                    = "https://cran.rstudio.com",
-  update_packages              = TRUE,
-  dependencies                 = TRUE,
-  lib                          = NULL,
-  check_xml_linux              = (R.Version()$os == "linux-gnu"),
-  check_libcurl_linux          = (R.Version()$os == "linux-gnu"),
-  check_openssl_linux          = (R.Version()$os == "linux-gnu"),
-  verbose                      = TRUE
+    url_package_dependencies,
+    cran_repo                    = "https://cran.rstudio.com",
+    update_packages              = TRUE,
+    dependencies                 = TRUE,
+    lib                          = NULL,
+    check_xml_linux              = (R.Version()$os == "linux-gnu"),
+    check_libcurl_linux          = (R.Version()$os == "linux-gnu"),
+    check_openssl_linux          = (R.Version()$os == "linux-gnu"),
+    verbose                      = TRUE
 ) {
-
+  
   # if( !file.exists(url_package_dependencies) )
   #   base::stop("The path `", url_package_dependencies, "` was not found.  Make sure the working directory is set to the root of the repository.")
-
+  
   # ---- load-sources ------------------------------------------------------------
-
+  
   # ---- load-packages -----------------------------------------------------------
-
+  
   # ---- declare-globals ---------------------------------------------------------
   required_columns <- c("package_name", "on_cran", "install", "github_username", "description")
-
+  
   # ---- load-data ---------------------------------------------------------------
   if (verbose)
     message("package_janitor is loading the list of package dependencies.")
-
+  
   if (!base::requireNamespace("checkmate"))
     utils::install.packages("checkmate", repos = cran_repo, lib = lib) # nocov
-
+  
   checkmate::assert_character(url_package_dependencies, min.chars = 1, len=1)
-
+  
   ds_packages <- utils::read.csv(
     file = url_package_dependencies,
     stringsAsFactors = FALSE
   )
-
+  
   rm(url_package_dependencies)
-
+  
   # ---- tweak-data --------------------------------------------------------------
   missing_columns <- base::setdiff(required_columns, colnames(ds_packages))
   if (1L <= length(missing_columns))
     stop(paste("The data.frame of the required packages is missing the following columns:", missing_columns))
-
+  
   ds_install_from_cran    <- ds_packages[ds_packages$install &  ds_packages$on_cran, ]
   ds_install_from_github  <- ds_packages[ds_packages$install & !ds_packages$on_cran & !is.na(ds_packages$github_username) & nchar(ds_packages$github_username)>0, ]
-
+  
   rm(ds_packages)
-
+  
   # ---- cran-packages -----------------------------------------------------------
   if (verbose)
     message("package_janitor is updating the existing packages from CRAN.")
   if (update_packages)
     utils::update.packages(ask = FALSE, checkBuilt = TRUE, repos = cran_repo) # nocov
-
+  
   # ---- install-pak -------------------------------------------------------------
   if (!base::requireNamespace("pak"))
     utils::install.packages("pak", repos = cran_repo, lib = lib) # nocov
-
+  
   # ---- install-cran-packages ---------------------------------------------------
   if (verbose)
     message("package_janitor is installing the CRAN packages:")
-
-  if (update_packages) {
-    pak::pak(ds_install_from_cran$package_name, lib = lib)
-  } else {
-    to_install <- ds_install_from_cran$package_name[
-      !vapply(ds_install_from_cran$package_name, base::requireNamespace, logical(1), quietly = TRUE)
-    ]
-    if (1L <= length(to_install))
-      pak::pak(to_install, lib = lib)
-  }
-
+  
+  pak::pkg_install(ds_install_from_cran$package_name, lib = lib, ask = FALSE)
+  
+  # if (update_packages) {
+  #   pak::pkg_install(ds_install_from_cran$package_name, lib = lib, ask = FALSE)
+  # } else {
+  #   to_install <- ds_install_from_cran$package_name[
+  #     !vapply(ds_install_from_cran$package_name, base::requireNamespace, logical(1), quietly = TRUE)
+  #   ]
+  #   if (1L <= length(to_install))
+  #     pak::pkg_install(to_install, lib = lib, ask = FALSE)
+  # }
+  
   rm(ds_install_from_cran)
   if (verbose)
     message("\n")
-
+  
   # ----check-linux-xml ---------------------------------------------------------
   #http://stackoverflow.com/questions/7765429/unable-to-install-r-package-in-ubuntu-11-04
-
+  
   if (check_xml_linux) {
     xml_results <- length(base::system2("locate", "r-cran-xml", stdout = TRUE))
     xml_missing <- (xml_results == 0L)
-
+    
     if (xml_missing)
       base::warning(
         "This Linux machine is possibly missing the 'r-cran-xml' library.  ",
         "Consider running `sudo apt-get install r-cran-xml` ",
         "or the equivalent for your distribution."
       )
-
+    
     base::rm(xml_results, xml_missing)
   }
-
+  
   # ---- check-linux-libcurl -----------------------------------------------------
   if (check_libcurl_linux) {
     libcurl_results <- length(base::system2("locate", "libcurl4", stdout = TRUE))
     libcurl_missing <- (libcurl_results == 0L)
-
+    
     if (libcurl_missing)
       base::warning(
         "This Linux machine is possibly missing the 'libcurl' library.  ",
         "Consider running `sudo apt-get install libcurl4-openssl-dev` ",
         "or the equivalent for your distribution."
       )
-
+    
     base::rm(libcurl_results, libcurl_missing)
   }
-
+  
   # ---- check-linux-openssl -----------------------------------------------------
   if (check_openssl_linux) {
     openssl_results <- length(base::system2("locate", "libssl-dev", stdout = TRUE))
     openssl_missing <- (openssl_results == 0L)
-
+    
     if (openssl_missing)
       base::warning(
         "This Linux machine is possibly missing the 'libssl' library.  ",
         "Consider running `sudo apt-get install libssl-dev` ",
         "or the equivalent for your distribution."
       )
-
+    
     base::rm(openssl_results, openssl_missing)
   }
-
+  
   #---- install-github-packages -------------------------------------------------
   if (verbose)
     message("\npackage_janitor is installing the GitHub packages:")
-
-  repository_names <- paste0(ds_install_from_github$github_username, "/", ds_install_from_github$package_name)
+  
+  repository_names <- paste0("github::", ds_install_from_github$github_username, "/", ds_install_from_github$package_name)
   if (1L <= length(repository_names))
-    pak::pak(repository_names, lib = lib)
-
+    pak::pkg_install(repository_names, lib = lib, ask = FALSE)
+  
   base::rm(ds_install_from_github, repository_names)
-
+  
   # ---- notify-tinytex ---------------------------------------------------------
   if (any(installed.packages() == "tinytex")) {
     # This comparison is copied from tinytex:::is_tinytex().
     if (!(gsub("^[.]", "", tolower(basename(tinytex::tinytex_root()))) == "tinytex"))
       tinytex::install_tinytex() # nocov
   }
-
+  
   # ---- return ---------------------------------------------------------
-
   if (verbose)
     message("package_janitor is complete.")
 }
